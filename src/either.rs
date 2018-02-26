fd37aaf92e3cebefd9ac715fd35c90206acf5a95
Edward Tate <cmalune@gmail.com>
2018-02-26 14:15:01 -0400

    Implemented Either specialised on <Value,Domain>.

--- src/either.rs
+++ src/either.rs
@@ -1,5 +1,18 @@
+use value::Value;
+use domain::Domain;
+
 #[derive(Clone, Debug)]
 pub enum Either<L, R> {
     Left(L),
     Right(R),
+}
+
+impl Either<Value, Domain> {
+    pub fn expect_value(&self) -> Value {
+        match *self {
+            Either::Left(ref l) => l.clone(),
+
+            Either::Right(_) => panic!("Expected left"),
+        }
+    }
 }
