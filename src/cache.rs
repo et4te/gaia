9aaaa9a9a328366ced570ac1fcb5a74b830efb27
Edward Tate <cmalune@gmail.com>
2018-03-15 16:11:50 -0400

    rustfmt cache.rs.

--- src/cache.rs
+++ src/cache.rs
@@ -14,24 +14,28 @@ pub struct Key {
 
 #[derive(Clone, Debug)]
 pub struct Cache {
-    pub cache: HashMap<Key,Either<Value,Domain>>,
+    pub cache: HashMap<Key, Either<Value, Domain>>,
 }
 
 impl Cache {
-
     pub fn new() -> Cache {
         Cache {
             cache: HashMap::new(),
         }
     }
 
-    pub fn find(&mut self, x: Identifier, k: Context) -> Option<&Either<Value,Domain>> {
-        self.cache.get(&Key{x: x.clone(), k: k})
+    pub fn find(&mut self, x: Identifier, k: Context) -> Option<&Either<Value, Domain>> {
+        self.cache.get(&Key { x: x.clone(), k: k })
     }
 
-    pub fn add(&mut self, x: Identifier, k: Context, v: Either<Value,Domain>) -> Either<Value,Domain> {
-        self.cache.insert(Key{x: x, k: k}, v.clone());
+    pub fn add(
+        &mut self,
+        x: Identifier,
+        k: Context,
+        v: Either<Value, Domain>,
+    ) -> Either<Value, Domain> {
+        // println!("Inserting {} {} = {:?}", x.clone(), k.print(), v.clone());
+        self.cache.insert(Key { x: x, k: k }, v.clone());
         v
     }
-
 }
